93f80b2d01990c76768ad2fd835c4ec64dbf9ab9
mpuskar2 <marko.p1211@gmail.com>
2022-11-05 22:04:30 -0400

    Added list creation to front end

--- static/script.js
+++ static/script.js
@@ -1,7 +1,8 @@
-document.getElementById('trackNS').addEventListener('click', getData)
+document.getElementById('trackNS').addEventListener('click', trackNameSearch);
+document.getElementById('albumNS').addEventListener('click', albumNameSearch);
+document.getElementById('createNewList').addEventListener('click', createList);
 
-function getData() {
-
+function trackNameSearch() {
     let trackName = document.getElementById('trackName');
     let filterName = trackName.value.toLowerCase();
 
@@ -10,13 +11,35 @@ function getData() {
         return;
     }
 
-    // Ensure that the search input is no more than 20 characters and does not contain numbers
+    // Ensure that the search input is no more than 20 characters
     if ((filterName.length > 20)) {
         trackName.value = "";
         return;
     }
 
-    let path = '/tracks/' + filterName;
+    getTrackData(filterName);
+}
+
+function albumNameSearch() {
+    let albumName = document.getElementById('albumName');
+    let filterName = albumName.value.toLowerCase();
+
+    // Don't search if search bar is empty
+    if (filterName == ""){
+        return;
+    }
+
+    // Ensure that the search input is no more than 20 characters
+    if ((filterName.length > 20)) {
+        albumName.value = "";
+        return;
+    }
+
+    getTrackData(filterName);
+}
+
+function getTrackData(textField) {
+    let path = '/tracks/' + textField;
 
     fetch(path)
     .then(res => res.json()
@@ -53,5 +76,53 @@ function getData() {
     })
     //catch
     )
+    .catch(err => console.log('Failed to find track or album of that name'))
     //catch
+}
+
+function createList() {
+    let listName = document.getElementById('listName');
+    let filterName = listName.value;
+    let trackIds = document.getElementById('trackIds');
+    let trackIdsTxt = trackIds.value;
+
+    // Don't create list if list name box is empty
+    if (filterName == ""){
+        return;
+    }
+
+    // Ensure that the list name input is no more than 20 characters
+    if ((filterName.length > 20)) {
+        listName.value = "";
+        return;
+    }
+
+    let trackArray = [];
+    if (trackIdsTxt != ""){
+        trackArray = trackIdsTxt.split(",");
+    }
+
+    console.log(trackArray);
+
+    let newList = {
+        tracks: trackArray
+    }
+
+    let path = '/lists/' + filterName;
+    fetch(path, {
+        method: 'PUT',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(newList)
+    })
+    .then(res => {
+        if (res.ok) {
+            res.json()
+            .then()
+            .catch(err => 'Failed to get json object')
+        }
+        else {
+            console.log('Error:', res.status);
+        }
+    })
+    .catch()
 }
