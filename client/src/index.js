e7aacb0f7cabcbcb5090f7b4267c1a91b1e88a48
Michael Ruiz <mruiz6@uwo.ca>
2022-11-30 21:57:36 -0500

    added authentication

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="michael-ruiz.us.auth0.com"
+    clientId="MFv08zdXjemknnbIy8GJGbRiucLIlnMC"
+    redirectUri={window.location.origin}>
     <App/>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
