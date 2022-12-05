fdc1bce3d14488801c4db4a83b64abc0c1f4dea6
mpuskar2 <marko.p1211@gmail.com>
2022-12-05 18:07:04 -0500

    Added list creation, updating and deleting

--- client/src/components/ListsSection.js
+++ client/src/components/ListsSection.js
@@ -22,9 +22,43 @@ export function ListsSectionLogin() {
   return (
     <>
     <div className={user == undefined ? "hidden" : ""}>
-        <input type="text" placeholder="List Name"/>
-        <button>Create</button>
+        <h2>Manage Lists</h2>
     </div>
+    <div className={user == undefined ? "hidden" : ""}>
+            <h3>Create List</h3>
+            <input id="listName" type="text" placeholder="List Name"/>
+            <input id="trackIds" type="text" placeholder="Track IDs"/>
+            <input id="desc" type="text" placeholder="Description (Optional)"/>
+            <input type="checkbox" id="vis" name="vis"/>
+            <label htmlFor="vis">Public?</label><br/>
+            <button onClick={() => createList(user.email)} id="createNewList">Create List</button>
+            <p>Input no more than 20 characters for name and numbers separated by commas for IDs</p>
+      </div>
+      <div className={user == undefined ? "hidden" : ""}>
+            <h3>Update List</h3>
+            <input id="listName2" type="text" placeholder="List Name"/>
+            <input id="trackIds2" type="text" placeholder="Track IDs"/>
+            <input id="desc2" type="text" placeholder="Description (Optional)"/>
+            <input type="checkbox" id="vis2" name="vis2"/>
+            <label htmlFor="vis2">Public?</label><br/>
+            <button onClick={() => updateList(user.email)} id="updateList">Update List</button>
+            <p>Input no more than 20 characters for name and numbers separated by commas for IDs</p>
+      </div>
+      <div className={user == undefined ? "hidden" : ""}>
+            <h3>Delete List</h3>
+            <input id="listName3" type="text" placeholder="List Name"/>
+            <button onClick={() => deleteList(user.email)} id="deleteList">Delete List</button>
+            <p>Input no more than 20 characters for name</p>
+      </div>
+      <div>
+        <h2>My Lists</h2>
+        <button onClick={getMyLists}>View My Lists</button>
+      </div>
+      <div>
+        <ul id="userListList">
+          <li><b>List Name | Number of Tracks | Runtime</b></li>
+        </ul>
+      </div>
     </>
   )
 }
@@ -87,8 +121,6 @@ function getAllLists() {
                 })
                 )
               })
-
-
             }
 
             li.appendChild(p);
@@ -108,4 +140,212 @@ function compareDate(a, b){
     return 1;
   }
   return 0;
+}
+
+function createList(email) {
+  let listName = document.getElementById('listName');
+  let filterName = listName.value;
+  let trackIds = document.getElementById('trackIds');
+  let trackIdsTxt = trackIds.value;
+  let desc = document.getElementById('desc');
+  let descTxt = desc.value;
+  let vis = document.getElementById('vis');
+  let visTxt;
+
+  // Don't create list if list name box is empty
+  if (filterName == ""){
+      return;
+  }
+
+  // Ensure that the list name input is no more than 20 characters and that the ids don't contain letters
+  if ((filterName.length > 20) || containsLetter(trackIdsTxt)) {
+      listName.value = "";
+      trackIds.value = "";
+      return;
+  }
+
+  let trackArray = [];
+  if (trackIdsTxt != ""){
+      trackArray = trackIdsTxt.split(",");
+  }
+
+  if (vis.checked) {
+    visTxt = "public";
+  }
+  else {
+    visTxt = "private";
+  }
+
+  let d1 = new Date();
+  let date = `${d1.getDate()}-${d1.getMonth() + 1}-${d1.getFullYear()}`;
+
+  let newList = {
+      tracks: trackArray,
+      description: descTxt,
+      creator: email,
+      visibility: visTxt,
+      rating: "",
+      lastModified: date
+  }
+
+  let path = '/lists/' + filterName;
+  fetch(path, {
+      method: 'PUT',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(newList)
+  })
+  .then(res => {
+      if (res.ok) {
+          res.json()
+          .then()
+          .catch(err => 'Failed to get json object')
+      }
+      else {
+          alert("Error! Check:\nYour list name\nYour track IDs are valid");
+          console.log('Error:', res.status);
+      }
+  })
+  .catch()
+}
+
+async function updateList(email) {
+  let listName = document.getElementById('listName2');
+  let filterName = listName.value;
+  let trackIds = document.getElementById('trackIds2');
+  let trackIdsTxt = trackIds.value;
+  let desc = document.getElementById('desc2');
+  let descTxt = desc.value;
+  let vis = document.getElementById('vis2');
+  let visTxt;
+  let creator;
+
+  // Don't create list if list name box is empty
+  if (filterName == ""){
+      return;
+  }
+
+  // Ensure that the list name input is no more than 20 characters and that the ids don't contain letters
+  if ((filterName.length > 20) || containsLetter(trackIdsTxt)) {
+      listName.value = "";
+      trackIds.value = "";
+      return;
+  }
+
+  let trackArray = [];
+  if (trackIdsTxt != ""){
+      trackArray = trackIdsTxt.split(",");
+  }
+
+  if (vis.checked) {
+    visTxt = "public";
+  }
+  else {
+    visTxt = "private";
+  }
+
+  let d1 = new Date();
+  let date = `${d1.getDate()}-${d1.getMonth() + 1}-${d1.getFullYear()}`;
+
+  let newList = {
+    tracks: trackArray,
+    description: descTxt,
+    creator: email,
+    visibility: visTxt,
+    rating: "",
+    lastModified: date
+}
+
+  let path = '/lists/' + filterName;
+
+  // Get creator from list and check that it matches logged in user first
+  await fetch(path)
+  .then(res2 => res2.json()
+  .then(data1 => {
+    creator = data1.creator;
+  })
+  )
+  .catch(err => console.log('Failed to find list of that name'))
+
+  if (email === creator){
+    fetch(path, {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(newList)
+    })
+    .then(res => {
+      if (res.ok) {
+          res.json()
+          .then()
+          .catch(err => 'Failed to get json object')
+      }
+      else {
+          alert("Error! Check:\nIf your list exists\nYour track IDs are valid");
+          console.log('Error:', res.status);
+      }
+    })
+    .catch()
+  }
+  else {
+    alert("Cannot edit another user's list!");
+  }
+}
+
+async function deleteList(email) {
+  let listName = document.getElementById('listName3');
+  let filterName = listName.value;
+  let creator;
+
+  // Don't create list if list name box is empty
+  if (filterName == ""){
+      return;
+  }
+
+  // Ensure that the list name input is no more than 20 characters
+  if ((filterName.length > 20)) {
+      listName.value = "";
+      return;
+  }
+  
+
+  let path = '/lists/' + filterName;
+
+  // Get creator from list and check that it matches logged in user first
+  await fetch(path)
+  .then(res2 => res2.json()
+  .then(data1 => {
+    creator = data1.creator;
+  })
+  )
+  .catch(err => console.log('Failed to find list of that name'))
+
+  if (email === creator){
+    fetch(path, {
+      method: 'DELETE',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify()
+    })
+    .then(res => {
+      if (res.ok) {
+          res.json()
+          .then()
+          .catch(err => 'Failed to get json object')
+      }
+      else {
+          alert("List does not exist!");
+          console.log('Error:', res.status);
+      }
+    })
+    .catch()
+  }
+  else {
+    alert("Cannot delete another user's list!");
+  }
+}
+
+function containsLetter(str) {
+  return ((/[a-z]/.test(str)) || (/[A-Z]/.test(str)));
+}
+
+function getMyLists(){
+
 }
